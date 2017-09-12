678b32708f7dca451d5a96c67063bfafa320ac31
ritchfunes <ritchfunes10@gmail.com>
2017-09-12 11:53:03 -0600

    first commit

---
+++ contah/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(contah.Startup))]
+namespace contah
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
